a29fbe9f80a03e519fb7206249350ccfe77155ac
cozyGarage <personal.trungnguyen@gmail.com>
2023-04-08 15:55:56 +0300

    view js

--- js/view.js
+++ js/view.js
@@ -1,18 +1,19 @@
 class View{
-    $ = {}
+    $ = {};
+    
     constructor() {
-        this.$.menu = this.#qs('[data-id="menu"]'),
-        this.$.menuBtn = this.#qs('[data-id="menu-btn"]'),
-        this.$.menuItems = this.#qs('[data-id="menu-items"]'),
-        this.$.resetBtn = this.#qs('[data-id="reset-btn"]'),
-        this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]'),
-        this.$.modal = this.#qs('[data-id="modal"]'),
-        this.$.modalText = this.#qs('[data-id="modal-text"]'),
-        this.$.modalBtn = this.#qs('[data-id="modal-btn"]'),
-        this.$.turn = this.#qs('[data-id="turn"]'),
-        this.$.p1Wins = this.#qs('[data-id="p1-wins"]'),
-        this.$.p2Wins = this.#qs('[data-id="p2-wins"]'),
-        this.$.ties = this.#qs('[data-id="ties"]'),
-        this.$.grid = this.#qs('[data-id="grid"]'),
+        this.$.menu = this.#qs('[data-id="menu"]');
+        this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+        this.$.menuItems = this.#qs('[data-id="menu-items"]');
+        this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+        this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+        this.$.modal = this.#qs('[data-id="modal"]');
+        this.$.modalText = this.#qs('[data-id="modal-text"]');
+        this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+        this.$.turn = this.#qs('[data-id="turn"]');
+        this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
+        this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
+        this.$.ties = this.#qs('[data-id="ties"]');
+        this.$.grid = this.#qs('[data-id="grid"]');
     }
 }
