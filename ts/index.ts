e3d65f38d65a9a057ee81534bf3021e4e014fdc8
cozyGarage <e1500953@edu.vamk.fi>
2023-05-18 09:09:12 +0300

    test ts

---
+++ ts/index.ts
@@ -0,0 +1,9 @@
+type CustomObject = {
+    someVariable : number
+    anotherVariable : string
+};
+
+const someVariable: CustomObject = {
+    someVariable: 1,
+    anotherVariable: 'some string',
+};
