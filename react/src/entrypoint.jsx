72a0a4136b5584ef4f837f356255498fe9940c8d
cozyGarage <e1500953@edu.vamk.fi>
2023-05-18 13:22:51 +0300

    mitigate

--- react/src/entrypoint.jsx
+++ react/src/entrypoint.jsx
@@ -1,8 +1,12 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('react-root');
+const rootElement = document.getElementById("react-root");
 const root = createRoot(rootElement);
-root.render(<StrictMode>            
+
+root.render(
+  <StrictMode>
     <App />
-</StrictMode>);
+  </StrictMode>
+);
